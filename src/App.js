1054e469dd3c1fdcd2d616cb1fa8c860c5af7636
Gustavo Raeski <gustavoraeski@outlook.com>
2020-08-11 22:36:52 -0300

    Iniciando o form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Olá!</h1>
-  );
+  return <Main />;
 }
 
 export default App;
